77ef2e352f12b01db865182c2b30d73009b34781
sist <sist@DESKTOP-8UUB40D>
2022-05-25 16:55:11 +0900

    JavaScript

---
+++ JavaScriptStudy/src/main/webapp/_01_where_to/js/myScript.js
@@ -0,0 +1,4 @@
+function myFunction() {
+		document.getElementById("demo").innerHTML = '<span style="color:red;">p태그의 내용이 변경되었습니다.<span>';	/* 대소문자 구문 할 것 , p태그 가져오기. */
+		/*innerHTML 은 p태그 내용을 말하는 것. innerHTML 안에 = 뒤의 ""문구가 들어간다. */
+}
